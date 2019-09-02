9ffc34ee702993b7059f8a4b305e26f06a5da8e0
Edwildson <edwildson@hotmail.com>
2019-09-01 23:24:37 -0300

    teste statccc

--- catalog/tests/test_views.py
+++ catalog/tests/test_views.py
@@ -1,4 +1,21 @@
-from django.test import TestCase
+
+from django.test import TestCase, Client
 from model_mommy import mommy
 from catalog.models import Category, Product
 from django.core.urlresolvers import reverse
+
+
+
+class ProductListTestCase(TestCase):
+    def setUp(self):
+        self.url = reverse('catalog:product_list')
+        self.client = Client()
+        self.products = mommy.make('catalog.Product', _quantity=10)
+
+    def tearDown(self):
+        for p in self.products:
+            p.delete()
+    def test_view_ok(self):
+       response = self.client.get(self.url)
+       self.assertEquals(response.status_code,200)
+       self.assertTemplateUsed(response, 'catalog/product_list.html')
