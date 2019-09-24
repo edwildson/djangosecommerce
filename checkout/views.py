566ea8fbc468ee14235f1769c38364db04b69f3a
Edwildson <edwildson@hotmail.com>
2019-09-23 23:00:10 -0300

    pagamento

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import RedirectView, TemplateView
-from .models import CartItem
+from .models import CartItem, Order
 from catalog.models import Product
 from django.contrib import messages
 from django.forms import modelformset_factory
 from django.core.urlresolvers import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class CreateCartItemView(RedirectView):
@@ -54,6 +55,23 @@ class CartItemView(TemplateView):
             context['formset'] = self.get_formset(clear=True)
         return self.render_to_response(context)
 
+class CheckoutView(LoginRequiredMixin, TemplateView):
+
+    template_name = 'checkout/checkout.html'
+
+    def get(self, request, *args, **kwargs):
+        session_key = request.session.session_key
+        if session_key and CartItem.objects.filter(cart_key=session_key).exists():
+            cart_items = CartItem.objects.filter(cart_key=session_key)
+            order = Order.objects.create_order(
+                user=request.user, cart_items=cart_items
+            )
+        else:
+            messages.info(request, 'Não há itens no carrinho de compras')
+            return redirect('checkout:cart_item')
+        return super(CheckoutView, self).get(request, *args, **kwargs)
+
 
 create_cartitem = CreateCartItemView.as_view()
 cart_item = CartItemView.as_view()
+checkout = CheckoutView.as_view()
