f9a6d2fc7c5815b65ebdd642822c3af250c3cbc1
samuel_santana <ssankys3@gmail.com>
2021-05-16 16:50:01 -0300

    modificando a aparencia

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,6 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
